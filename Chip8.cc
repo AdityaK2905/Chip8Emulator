4697e1f3bf890ff354af45c12bfcfe00e141a548
Aditya Kshirsagar <aditya29@icloud.com>
2023-12-28 02:03:55 -0600

    Added more opcode instructions, still have to add a few and deal with Dxyn

--- Chip8.cc
+++ Chip8.cc
@@ -29,6 +29,11 @@ uint8_t fontset[FONTSET_SIZE] =
 
 Chip8::Chip8() {
     pc = START_ADDRESS;
+
+    for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+	{
+		memory[FONTSET_START_ADDRESS + i] = fontset[i];
+	}
 }
 
 
@@ -58,4 +63,310 @@ void Chip8::LoadROM(char const* filename)
 		// Free the buffer
 		delete[] buffer;
 	}
+}
+
+//OPCODES
+void Chip8::OP_00E0() {
+    memset(video, 0, sizeof(video));
+}
+
+
+void Chip8::OP_00EE() {
+    --sp;
+    pc = stack[sp];
+}
+
+void Chip8::OP_1nnn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+	pc = address;
+}
+
+void Chip8::OP_2nnn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+    stack[sp] = pc;
+	++sp;
+	pc = address;
+}
+
+void Chip8::OP_3xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t byte = opcode & 0x00FFu;
+
+	if (registers[Vx] == byte)
+	{
+		pc += 2;
+	}
+}
+
+void Chip8::OP_4xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t byte = opcode & 0x00FFu;
+
+	if (registers[Vx] != byte)
+	{
+		pc += 2;
+	}
+}
+
+void Chip8::OP_5xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] == registers[Vy]) {
+        pc += 2;
+    }
+}
+
+void Chip8::OP_6xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] = byte;
+}
+
+void Chip8::OP_7xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] = registers[Vx] + byte;
+}
+
+void Chip8::OP_8xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = registers[Vy];
+}
+
+void Chip8::OP_8xy1() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = (registers[Vx] | registers[Vy]);
+}
+
+void Chip8::OP_8xy2() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = (registers[Vx] & registers[Vy]);
+}
+
+void Chip8::OP_8xy3() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = (registers[Vx] ^ registers[Vy]);
+}
+
+void Chip8::OP_8xy4() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] + registers[Vy] > 255) {
+        registers[0xF] = 1; 
+    } else {
+        registers[0xF] = 0;
+    }
+
+    registers[Vx] = (registers[Vx] + registers[Vy]) & 0xFFu;
+}
+
+void Chip8::OP_8xy5() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    if (registers[Vx] > registers[Vy]) {
+        registers[0xF] = 1;
+        
+    } else {
+        registers[0xF] = 0;
+        
+    }
+    registers[Vx] -= registers[Vy];
+}
+
+void Chip8::OP_8xy6() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+	// Save LSB in VF
+	registers[0xF] = (registers[Vx] & 0x1u);
+
+	registers[Vx] >>= 1;
+}
+
+void Chip8::OP_8xy7() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    if (registers[Vy] > registers[Vx]) {
+        registers[0xF] = 1;
+        
+    } else {
+        registers[0xF] = 0;
+        
+    }
+    registers[Vx] = registers[Vy] - registers[Vx];
+}
+
+void Chip8::OP_8xyE() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    registers[0xF] = (registers[Vx] & 0x80u) >> 7u;
+
+	registers[Vx] <<= 1;
+}
+
+void Chip8::OP_9xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    
+    if (registers[Vx] != registers[Vy]) {
+        pc+=2;
+    }
+}
+
+void Chip8::OP_Annn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+	index = address;
+}
+
+void Chip8::OP_Bnnn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+	pc = registers[0] + address;
+}
+
+void Chip8::OP_Cxkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+	registers[Vx] = randByte(randGen) & byte;
+}
+
+//Finish this later
+void Chip8::OP_Dxyn() {
+    uint8_t n = opcode & 0x000Fu;
+
+    uint16_t address = index;
+
+}
+
+void Chip8::OP_Ex9E() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+	uint8_t key = registers[Vx];
+
+	if (keypad[key])
+	{
+		pc += 2;
+	}
+}
+
+void Chip8::OP_ExA1() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+	uint8_t key = registers[Vx];
+
+	if (!keypad[key])
+	{
+		pc += 2;
+	}
+}
+
+void Chip8::OP_Fx07() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    
+    registers[Vx] = delayTimer;
+}
+
+void Chip8::OP_Fx0A() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    if (keypad[0])
+	{
+		registers[Vx] = 0;
+	}
+	else if (keypad[1])
+	{
+		registers[Vx] = 1;
+	}
+	else if (keypad[2])
+	{
+		registers[Vx] = 2;
+	}
+	else if (keypad[3])
+	{
+		registers[Vx] = 3;
+	}
+	else if (keypad[4])
+	{
+		registers[Vx] = 4;
+	}
+	else if (keypad[5])
+	{
+		registers[Vx] = 5;
+	}
+	else if (keypad[6])
+	{
+		registers[Vx] = 6;
+	}
+	else if (keypad[7])
+	{
+		registers[Vx] = 7;
+	}
+	else if (keypad[8])
+	{
+		registers[Vx] = 8;
+	}
+	else if (keypad[9])
+	{
+		registers[Vx] = 9;
+	}
+	else if (keypad[10])
+	{
+		registers[Vx] = 10;
+	}
+	else if (keypad[11])
+	{
+		registers[Vx] = 11;
+	}
+	else if (keypad[12])
+	{
+		registers[Vx] = 12;
+	}
+	else if (keypad[13])
+	{
+		registers[Vx] = 13;
+	}
+	else if (keypad[14])
+	{
+		registers[Vx] = 14;
+	}
+	else if (keypad[15])
+	{
+		registers[Vx] = 15;
+	}
+	else
+	{
+		pc -= 2;
+	}
+}
+
+
+void Chip8::OP_Fx15() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    delayTimer = registers[Vx];
+}
+
+void Chip8::OP_Fx18() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    soundTimer = registers[Vx];
+}
+
+void Chip8::OP_Fx1E() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    index += registers[Vx];
 }
--- Chip8.hpp
+++ Chip8.hpp
@@ -27,4 +27,100 @@ public:
     Chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().count()) {
         randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
     };
+
+
+    //OPCODES
+
+    //CLS - Clears Screen
+    void OP_00E0();
+
+    //RET - Return from subroutine
+    void OP_00EE();
+
+    //JP addr - Jump to location nnn
+    void OP_1nnn();
+
+    //2nnn - CALL addr, Call subroutine at address nnn
+
+    void OP_2nnn();
+
+    //3xkk - Skips next instruction if register Vx = kk
+    void OP_3xkk();
+
+    //4xkk - Skip next instruction if Vx != kk.
+    void OP_4xkk();
+
+    //5xy0 - Skips next instruction if register Vx = Vy
+    void OP_5xy0();
+
+    //6xkk - Set the value kk into the register Vx
+    void OP_6xkk();
+
+    //7xkk - Add the value of kk to the value in Register Vx
+    void OP_7xkk();
+
+    //8xy0 - Stores the value of register Vy into Vx, Vx = Vy
+    void OP_8xy0();
+
+    //8xy1 - Set Vx = Vx OR Vy
+    void OP_8xy1();
+
+    //8xy2 - Set Vx = Vx AND Vy
+    void OP_8xy2();
+
+    //8xy3 - Set Vx = Vx XOR Vy
+    void OP_8xy3();
+
+    //8xy4 - Set Vx = Vx+Vy, VF = 1 if result > 8 bits(>255), otherwise 0
+    void OP_8xy4();
+
+    //8xy5 - Set Vx = Vx-Vy, If VX > Vy, VF = 1. Otherwise, VF = 0 and Vx = Vy-Vx
+    void OP_8xy5();
+
+    //8xy6 - If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+    void OP_8xy6();
+
+    //8xy7 - If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
+    void OP_8xy7();
+
+    //8xyE - If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+    void OP_8xyE();
+
+    //9xy0 - If Vx != Vy, skip pc by two
+    void OP_9xy0();
+
+    //Annn - The value of register I is set to nnn
+    void OP_Annn();
+
+    //Bnnn - The program counter is set to nnn plus the value of V0.
+    void OP_Bnnn();
+
+    //Cxkk - The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx.
+    void OP_Cxkk();
+
+    //Dxyn - Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+    void OP_Dxyn();
+
+    //Ex9E - Skip next instruction if key with the value of Vx is pressed.
+    void OP_Ex9E();
+
+    //ExA1 - Skip next instruction if key with the value of Vx is not pressed.
+    void OP_ExA1();
+
+    //Fx07 - The value of DT is placed into Vx.
+    void OP_Fx07();
+
+    //Fx0A - Wait for a key press, store the value of the key in Vx.
+    void OP_Fx0A();
+
+    //Fx15 - DT is set equal to the value of Vx.
+    void OP_Fx15();
+
+    //Fx18 - ST is set equal to the value of Vx.
+    void OP_Fx18();
+
+    //Fx1E - The values of I and Vx are added, and the results are stored in I.
+    void OP_Fx1E();
+
+
 };
